9bc90485f76beb5c0942dadde6e616699c45a1dc
PsyChoBoyJAck83 <georgitsis.theodoros@gmail.com>
2023-11-22 23:36:11 +0100

    reloads when resizedImages.length and allImageEntires.length is not equal

--- src/app/thumbnail-view/thumbnail-view.component.ts
+++ src/app/thumbnail-view/thumbnail-view.component.ts
@@ -7,19 +7,35 @@ import { FetchApiDataService } from '../fetch-api-data.service';
   styleUrls: ['./thumbnail-view.component.scss'],
 })
 export class ThumbnailViewComponent implements OnInit {
+  baseUrl: string = '';
   allImageEntries: any[] = [];
+  resizedImageList: any[] = [];
 
   constructor(public fetchApiData: FetchApiDataService) {}
 
   ngOnInit(): void {
+    this.baseUrl = this.fetchApiData.returnUrl();
     this.loadAllImageEntries();
+    this.loadResizedImageList();
+    if (this.allImageEntries.length !== this.resizedImageList.length) {
+      this.loadAllImageEntries();
+      this.loadResizedImageList();
+    }
+
     this.fetchApiData.refreshNeeded$.subscribe(() => {
       this.loadAllImageEntries();
     });
   }
+
   private loadAllImageEntries(): void {
     this.fetchApiData.getAll().subscribe((resp: any) => {
       this.allImageEntries = resp;
+    });
+  }
+
+  private loadResizedImageList(): void {
+    this.fetchApiData.getResizedImages().subscribe((resp: any) => {
+      this.resizedImageList = resp;
     });
   }
 }
